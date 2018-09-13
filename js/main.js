0d17e44b076dd53b9357850531454e6a4d4ed344
Carlos de Carvalho <carloswdecarvalho@outlook.com>
2018-09-12 21:28:10 -0300

    First content div almost ready.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-// Loads only the first DIV after document loads
+/******* Loading Document ******/
 $(document).ready(function() {
-	$('#content-hello-left').first().removeClass('boxHidden');
-	$('#content-hello-left').first().addClass('boxLoaded');
+
+	// Animates the blue rectangle...
+	setTimeout(function() {
+		$('#content-hello-left').removeClass('boxHidden').addClass('boxLoaded');
+	}, 0);
+
+	// ...shows the "hello" message.
+	setTimeout(function() {
+		$('#content-hello-left').find('.boxTitle').removeClass('textHidden').addClass('textShow');
+	}, 1000);
+
+});
+
+/** Loads DIV's on scroll down */
+$(window).on('scroll', function() {
+
+	var scrollHeight = $(document).height();
+	var scrollPosition = $(window).height() + $(window).scrollTop();
+
+	if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+		alert('Testing...');
+	}
+
 });
