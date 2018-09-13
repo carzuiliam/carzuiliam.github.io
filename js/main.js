0693dcbfe585bcf7fcbba87a3f507496f885e538
Carlos de Carvalho <carloswdecarvalho@outlook.com>
2018-09-13 00:31:18 -0300

    First steps for loading div's dinamically.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-/******* Loading Document ******/
+/*************************** Loading document **************************/
+
 $(document).ready(function() {
 
 	// Animates the blue rectangle...
@@ -13,7 +14,19 @@ $(document).ready(function() {
 
 });
 
-/** Loads DIV's on scroll down */
+/************************** Scrolling document *************************/
+
 $(window).on('scroll', function() {
 
+	//var docViewTop = $(window).scrollTop();
+    //var docViewBottom = docViewTop + $(window).height();
+
+    //var elemTop = $('#content-info').offset().top;
+    //var elemBottom = elemTop + $('#content-info').height();
+
+    //if ((elemTop <= docViewBottom) && (elemBottom >= docViewTop)) 
+    //	alert('foi');
+
 });
+
+/****************************** Functions ******************************/
